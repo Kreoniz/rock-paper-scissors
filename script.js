f03ca2aa7b63999b4372b5e432f2db4b753f8c59
Kreoniz <kreoniz@mail.ru>
2022-12-08 21:46:56 +0600

    Add console-based rock-paper-scissrors game
    In this commit, I created multiple functions to let the user play
    the rock-paper-scissrors game. The user is prompted to make a move,
    after which the outcome is shown in the console.

---
+++ script.js
@@ -0,0 +1,56 @@
+function getComputerChoice() {
+    const turns = ['Rock', 'Paper', 'Scissors'];
+
+    const randomIndex = Math.floor(Math.random() * turns.length);
+    return turns[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return {
+            message: `It's a Tie! You both picked ${playerSelection}`,
+            score: 0.5
+        };
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'
+    || playerSelection === 'Paper' && computerSelection === 'Rock'
+    || playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return {
+            message: `You Win! ${playerSelection} beats ${computerSelection}`,
+            score: 1
+        };
+    } else if (computerSelection === 'Rock' && playerSelection === 'Scissors'
+    || computerSelection === 'Paper' && playerSelection === 'Rock'
+    || computerSelection === 'Scissors' && playerSelection === 'Paper'
+    ) {
+        return {
+            message: `You Lose! ${computerSelection} beats ${playerSelection}`,
+            score: 0
+        };
+    }
+}
+
+function game() {
+    let userScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Your turn:');
+        const computerSelection = getComputerChoice();
+        
+        const result = playRound(playerSelection, computerSelection);
+
+        console.log(result.message);
+        userScore += result.score;
+    }
+
+    if (userScore / 2 > 5 / 2) {
+        console.log(`You Win! Your score is ${userScore}`);
+    } else if (userScore / 2 < 5 / 2) {
+        console.log(`You Lose! Your score is ${userScore}`);
+    } else if (userScore / 2 === 5 / 2) {
+        console.log(`It's a Tie! Your score is ${userScore}`);
+    }
+}
+
+game()
